aa325afa820391acca66b6d4d422d735a2668e94
mknyambura <kariuki.mercie@gmail.com>
2022-12-19 21:00:56 +0300

    update commit

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,18 +1,33 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'f7ebe41667msh2f29f080e585f2bp14b0b2jsn9e72e33ee4ab',
-      'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
-    }
-};
-  
-fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+// const options = {
+//     method: 'GET',
+//     headers: {
+//       'X-RapidAPI-Key': 'f7ebe41667msh2f29f080e585f2bp14b0b2jsn9e72e33ee4ab',
+//       'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+//     }
+// };
+
+// fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
 
 export const shazamCoreApi = createApi({
-    reducerPath: 'shazamCoreApi',
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', 'f7ebe41667msh2f29f080e585f2bp14b0b2jsn9e72e33ee4ab');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query:  () => '/charts/world' }),
+  }),
 });
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;

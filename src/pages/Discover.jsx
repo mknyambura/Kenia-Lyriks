a823f2bdd5cf5af9eae2a21a4838d6d9a773d13e
mknyambura <kariuki.mercie@gmail.com>
2022-12-20 14:02:45 +0300

    update commit

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,14 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
-  console.log(genres);
+  // console.log(genres);
+
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = 'Pop';
 
@@ -31,6 +36,9 @@ const Discover = () => {
           <SongCard 
           key={song.key}
           song={song}
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          data={data}
           i={i}/>
         ))}
       </div>

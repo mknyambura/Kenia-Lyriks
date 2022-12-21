09487c426c5156a40cdf3547f054c47a11f35048
mknyambura <kariuki.mercie@gmail.com>
2022-12-21 10:00:43 +0300

    update commit

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
@@ -6,60 +7,60 @@ import { setActiveSong, playPause } from '../redux/features/playerSlice';
 import { useGetSongDetailsQuery, useGetSongRelatedQuery } from '../redux/services/shazamCore';
 
 const SongDetails = () => {
-    const dispatch = useDispatch();
-    const {songid, id: artistId } = useParams();
+  const dispatch = useDispatch();
+  const { songid, id: artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
 
-    const { activeSong, isPlaying } = useSelector((state) => state.player);
-    const { data, isFetching: isFetchinRelatedSongs, error } = useGetSongRelatedQuery({ songid });
-    const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
+  const { data, isFetching: isFetchinRelatedSongs, error } = useGetSongRelatedQuery({ songid });
+  const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
 
-    if (isFetchingSongDetails && isFetchinRelatedSongs) return <Loader title="Searching song details" />;
+  if (isFetchingSongDetails && isFetchinRelatedSongs) return <Loader title="Searching song details" />;
 
-    // console.log(songid);
-    // console.log(songData);
+  console.log(songData);
 
-    if (error) return <Error />;
+  if (error) return <Error />;
 
-    const handlePauseClick = () => {
-        dispatch(playPause(false));
-    };
-    
-    const handlePlayClick = (song, i) => {
-      dispatch(setActiveSong({ song, data, i }));
-      dispatch(playPause(true));
-    };
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
 
-    return (
-        <div className="flex flex-col">
-            <DetailsHeader
-            artistId={artistId}
-            songData={songData}
-            />
-    
-            <div className="mb-10">
-                <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
-        
-                <div className="mt-5">
-                    {songData?.sections[1].type === 'LYRICS'
-                      ? songData?.sections[1]?.text.map((line, i) => (
-                        <p key={`lyrics-${line}-${i}`} className="text-gray-400 text-base my-1">{line}</p>
-                      ))
-                      : (
-                        <p className="text-gray-400 text-base my-1">Sorry, No lyrics found!</p>
-                    )}
-                </div>
-            </div>
-    
-            <RelatedSongs
-              data={data}
-              artistId={artistId}
-              isPlaying={isPlaying}
-              activeSong={activeSong}
-              handlePauseClick={handlePauseClick}
-              handlePlayClick={handlePlayClick}
-            />
-    
+  const handlePlayClick = (song, i) => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader
+        artistId={artistId}
+        songData={songData}
+      />
+
+      <div className="mb-10">
+        <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
+
+        <div className="mt-5">
+          {songData?.sections[1].type === 'LYRICS'
+            ? songData?.sections[1]?.text.map((line, i) => (
+              <p key={`lyrics-${line}-${i}`} className="text-gray-400 text-base my-1">{line}</p>
+            ))
+            : (
+              <p className="text-gray-400 text-base my-1">Sorry, No lyrics found!</p>
+            )}
         </div>
-    );
-}
+      </div>
+
+      <RelatedSongs
+        data={data}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePauseClick={handlePauseClick}
+        handlePlayClick={handlePlayClick}
+      />
+
+    </div>
+  );
+};
+
 export default SongDetails;

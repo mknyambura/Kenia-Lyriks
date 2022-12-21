bd6ca3cc71d2fc685b62d224e0882d693f384907
mknyambura <kariuki.mercie@gmail.com>
2022-12-21 22:41:17 +0300

    update commit

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -39,7 +39,7 @@ const SongDetails = () => {
                 <h2 className="text-white text-3xl font-bold">Lyrics:</h2> 
                 <div className='mt-5'>
                     {songData?.sections[1].type === 'LYRICS' ? songData?.sections[1].text.map((line, i) => (
-                        <p className="text-gray-400 text-base my-1">{line}</p>
+                        <p key={`${line}-${i}`} className="text-gray-400 text-base my-1">{line}</p>
                     )) : (<p className="text-gray-400 text-base my-1">Sorry, no lyrics found!</p>)}
                 </div> 
             </div>

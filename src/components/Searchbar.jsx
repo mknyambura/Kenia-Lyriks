8dd24bbb7ed8010e1c438a29bf1e62004957e1fb
mknyambura <kariuki.mercie@gmail.com>
2022-12-22 13:35:06 +0300

    completed

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,23 +3,37 @@ import { useNavigate } from 'react-router-dom';
 
 import { FiSearch } from 'react-icons/fi';
 
-const Searchbar = () => (
-  <form 
-    autoComplete="off"
-    className="p-2 text-gray-400 focus-within:text-gray-600"
-  >
-    <label htmlFor="search-field" className="sr-only">Search all songs</label>
-    <div className="flex flex-row justify-start items-center">
-      <FiSearch aria-hidden="true" className="w-5 h-5 ml-4"/>
-      <input 
-        type="search"
-        name="search-field"
-        autoComplete="off"
-        id="search-field"
-        placeholder="Search"
-        className="flex-1 bg-transparent border-none placeholder-gray-500 outline-none text-base text-white p-4" />
-    </div>
-  </form>
-);
+const Searchbar = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    navigate(`/search/${searchTerm}`);
+  };
+
+  return (
+    <form 
+      autoComplete="off"
+      className="p-2 text-gray-400 focus-within:text-gray-600"
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="search-field" className="sr-only">Search all songs</label>
+      <div className="flex flex-row justify-start items-center">
+        <FiSearch aria-hidden="true" className="w-5 h-5 ml-4"/>
+        <input 
+          type="search"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          name="search-field"
+          autoComplete="off"
+          id="search-field"
+          placeholder="Search"
+          className="flex-1 bg-transparent border-none placeholder-gray-500 outline-none text-base text-white p-4" />
+      </div>
+    </form>
+  )
+};
 
 export default Searchbar;

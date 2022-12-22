df730527e3958e036a852cf28de47d4ac7f00118
mknyambura <kariuki.mercie@gmail.com>
2022-12-22 13:15:32 +0300

    update commit

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -4,8 +4,12 @@ const ArtistCard = ({track}) => {
   const navigate = useNavigate();
 
   return(
-    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+    <div 
+      className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"
+      onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}
+    >
       <img alt="song image" src={track?.images?.coverart} className="w-full h-56 rounded-lg" />
+      <p className="mt-4 font-semibold text-lg text-white truncate">{track?.subtitle}</p>
     </div>
   );
   
